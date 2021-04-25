96bbcbf8a22013a68c9adcc2446eef6a30fc607c
wadsongarbes <nenogarbes@hotmail.com>
2021-04-24 22:43:18 -0300

    Adiciona users, posts e cria base template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 from app import app
+from flask import render_template
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return 'Hello, World'
+    user = {'username': 'Wadson'}
+    posts = [
+        {
+            'author': {'username': 'Maria'},
+            'body': 'Lindo dia aqui na praia!'
+        },
+        {
+            'author': {'username': 'José'},
+            'body': 'Assistimos ao jogo do flamengo ontem!'
+        }
+
+    ]
+    return render_template('index.html', title="Home", user=user, posts=posts)
